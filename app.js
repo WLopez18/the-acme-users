67701018d1ed41c202b606f78e3aeb6aaa177186
Walter <walter18lopez@gmail.com>
2022-11-16 12:30:08 -0500

    Added Load Company Name button

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
 //https://www.acme-api.com/api/companies
 let loadButton = document.querySelector('#loadButton');
 let loadRandomButton = document.querySelector('#loadRandomButton');
-console.log(loadRandomButton);
+let loadCompanyBtn = document.querySelector('#loadCompanyName');
 let list = document.querySelector('#usersList');
 let h1 = document.querySelector('#randomH1');
-console.log(h1);
+let companyList = document.querySelector('#companyNames');
 
-loadButton.addEventListener('click', function(){
+loadButton.addEventListener('click', function () {
   loadUsers();
 });
 
-loadRandomButton.addEventListener('click', function(){
+loadRandomButton.addEventListener('click', function () {
   loadRandomUser();
 });
 
-async function loadRandomUser(){
+loadCompanyBtn.addEventListener('click', () => {
+  loadCompany();
+})
+
+async function loadRandomUser() {
   let response = await fetch('https://www.acme-api.com/api/users/random');
   let user = await response.json();
   randomH1.innerText = user.fullName;
 }
 
-async function loadUsers(){
+async function loadUsers() {
   let response = await fetch('https://www.acme-api.com/api/users');
   let data = await response.json();
   let users = data.users;
-  for(let i = 0; i < users.length; i++){
+  for (let i = 0; i < users.length; i++) {
     let user = users[i];
     let li = document.createElement('li');
     li.innerText = user.fullName;
     list.appendChild(li);
+  }
+}
+
+async function loadCompany() {
+  let response = await fetch('https://www.acme-api.com/api/companies');
+  let data = await response.json();
+  for (let key in data) {
+    let companies = data[key].name;
+    let li = document.createElement('li');
+    li.innerText = companies;
+    companyList.appendChild(li);
   }
 }
 
